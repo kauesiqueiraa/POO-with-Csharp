2536a53beaac454ce945c63b0978ce77fd67b11e
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-12-12 20:39:35 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Music.cs
@@ -0,0 +1,23 @@
+﻿class Music
+{
+    public string Name { get; set; }
+    public string Artist { get; set; }
+    public int Duration { get; set; }
+    public bool Disponible { get; set; }
+    public string Description => $"A Música {Name} pertence a banda {Artist}";
+
+    public void DisplayTechnicalSheet()
+    {
+        Console.WriteLine($"\nNome: {Name}");
+        Console.WriteLine($"Artista: {Artist}");
+        Console.WriteLine($"Duração: {Duration}");
+        if (Disponible)
+        {
+            Console.WriteLine("Essa Musica está disponível no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus++");
+        }
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,15 @@
+﻿Music music1 = new Music();
+music1.Name = "Lobo Guará";
+music1.Artist = "Hungria";
+music1.Duration = 271;
+music1.Disponible = true;
+Console.WriteLine(music1.Description);
+
+Music music2 = new Music();
+music2.Name = "Lembranças";
+music2.Artist = "Hungria";
+music2.Duration = 367;
+music2.Disponible = false;
+
+music1.DisplayTechnicalSheet();
+music2.DisplayTechnicalSheet();
